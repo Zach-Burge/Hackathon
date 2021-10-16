6ca7a5d2a307f13325c389ff09cbb141938a869a
Colebob1227 <colebob27@gmail.com>
2021-10-16 13:44:49 -0700

    try 2 again

--- background.js
+++ background.js
@@ -4,3 +4,8 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) { return { cancel: true }; },
+    { urls: ["*://*.doubleclick.net/*"] },
+    ["blocking"]
+);
