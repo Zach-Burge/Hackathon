6812c3c34fd48f91d17b7aad6f1b5899b82011e6
Colebob1227 <colebob27@gmail.com>
2021-10-16 13:41:19 -0700

    double click block

--- background.js
+++ background.js
@@ -4,3 +4,9 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) { return { cancel: true }; },
+    { urls: ["*://*.doubleclick.net/*"] },
+    ["blocking"]
+);
