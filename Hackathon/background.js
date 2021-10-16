df1d202f3b406ccaa545f4058f073845798d25d9
Zach Burge <zburge@zagmail.gonzaga.edu>
2021-10-16 14:27:22 -0700

    Basically Finished

---
+++ Hackathon/background.js
@@ -0,0 +1,12 @@
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color });
+    console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return {cancel: true}; },
+    { urls: ["*://*.doubleclick.net/*", "*://*.connatix.com/*", "*://*.secure.adnxs.com/*"]},
+    ["blocking"]
+);
