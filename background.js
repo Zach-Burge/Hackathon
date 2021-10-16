8b870a7e5aa86ea7a8ea7d0dfd83771d91d7c2d1
Colebob1227 <colebob27@gmail.com>
2021-10-16 13:44:12 -0700

    Revert "double click block"
    This reverts commit 6812c3c34fd48f91d17b7aad6f1b5899b82011e6.

--- background.js
+++ background.js
@@ -4,9 +4,3 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
 });
-
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details) { return { cancel: true }; },
-    { urls: ["*://*.doubleclick.net/*"] },
-    ["blocking"]
-);
